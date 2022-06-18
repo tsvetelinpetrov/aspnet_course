e42ebe34211ad3b2870b42be2c6dc98570e39e03
Tsvetelin Petrov <42267885+tsvetelinpetrov@users.noreply.github.com>
2022-06-19 02:56:33 +0300

    Implemented login form

---
+++ RegisterAndLogin/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegisterAndLogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegisterAndLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            if(userModel.username == "tsetso" && userModel.password == "tsetso123")
+            {
+                return View("LoginSuccess", userModel);
+            } else
+            {
+                return View("LoginFailure", userModel);
+            }
+
+            
+        }
+    }
+}
