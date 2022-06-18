633a2813e6275cf33e6a446435f0c558fe51a312
Tsvetelin Petrov <42267885+tsvetelinpetrov@users.noreply.github.com>
2022-06-19 02:17:39 +0300

    Implemented the controller basics

---
+++ Controllers/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Controllers.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Message()
+        {
+            return View("Message");
+        }
+
+        public IActionResult Welcome(string name, int number)
+        {
+            return View();
+        }
+    }
+}
