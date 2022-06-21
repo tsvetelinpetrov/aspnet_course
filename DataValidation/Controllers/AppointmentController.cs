63128569b2e68f64c173208fae4235349c964aa8
Tsvetelin Petrov <42267885+tsvetelinpetrov@users.noreply.github.com>
2022-06-22 00:28:50 +0300

    Implemented appointment controller & create view

---
+++ DataValidation/Controllers/AppointmentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DataValidation.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
