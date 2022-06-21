3b960c1f2f5c72cab4011b770b2af73fff5eb594
Tsvetelin Petrov <42267885+tsvetelinpetrov@users.noreply.github.com>
2022-06-22 00:07:17 +0300

    Added DataValidation project

---
+++ DataValidation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DataValidation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
